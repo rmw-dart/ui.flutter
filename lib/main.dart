6382d53834102fd4d6f211ee26dbafdb936d1858
gcxfd <i@rmw.link>
2021-10-22 18:14:27 +0800

    🔶

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,14 @@ import 'package:kraken/kraken.dart';
 import 'package:kraken_devtools/kraken_devtools.dart';
 import 'const.dart';
 import 'dart:ui' show window;
+import 'package:flutter/services.dart';
 
 const COLOR_WHITE = Color(0xffffffff);
 
 void main() {
+  // 设置顶部状态栏颜色
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: COLOR_WHITE));
   runApp(Init());
 }
 
