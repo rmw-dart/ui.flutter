7ac91918de4648a72d2c02e13b730edd49488c04
gcxfd <i@rmw.link>
2021-10-22 14:46:59 +0800

    🔶

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_macos
+import jsc
+import kraken
+import kraken_devtools
+import shared_preferences_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  JscPlugin.register(with: registry.registrar(forPlugin: "JscPlugin"))
+  KrakenPlugin.register(with: registry.registrar(forPlugin: "KrakenPlugin"))
+  KrakenDevtoolsPlugin.register(with: registry.registrar(forPlugin: "KrakenDevtoolsPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
